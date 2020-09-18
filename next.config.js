df762266b5f0065137c37e5c5a861da85b8ae50b
ibadeeCodes <ibadeeshaikh@gmail.com>
2020-09-18 09:49:11 -0700

    re deploy

--- next.config.js
+++ next.config.js
@@ -3,25 +3,4 @@ module.exports = {
     mongo_URI:
       'mongodb+srv://ibad:ibad123@cluster0.l9cif.mongodb.net/<dbname>?retryWrites=true&w=majority',
   },
-  async headers() {
-    return [
-      {
-        // mathching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ]
-  },
 }
